14a53e0bfae5d415819938db0226c8a9b23fddf2
VishnuGanapathy <150116834+VishnuGanapathy@users.noreply.github.com>
2023-11-08 12:15:07 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javaproject.calc;
+
+/**
+ *
+ * @author vishn
+ */
+public class Calc {
+
+    public static void main(String[] args) {
+        calculator calculator = new calculator();
+       System.out.println("success created simple calculator");
+    }
+}
